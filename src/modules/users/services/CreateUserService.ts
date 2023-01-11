75c649be18b9dd22a54a2a519c08104358169034
wellSantos1334 <well.santos1334@gmail.com>
2023-01-11 18:28:07 -0300

    fix: add hash password with bcrypt on createUser

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { IUser, IUserCreate } from '../infra/interfaces/IUser';
 import { UserRepository } from '../infra/typeorm/repositories/UsersRepository';
 import { IUserRepository } from '../interfaces/IUserRepository';
+import { hash } from 'bcryptjs';
 export default class CreateUserService {
   public async execute({ name, email, password, avatar }: IUserCreate): Promise<IUser> {
     const userRepository: IUserRepository = new UserRepository();
@@ -16,10 +17,12 @@ export default class CreateUserService {
       throw new AppError('Email already exists');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const newUser = await userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
       avatar,
     });
 
